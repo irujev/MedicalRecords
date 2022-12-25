05574ff30396f7d07592c2c73488eb775b6b1873
Ivan Ruzhev <irujev@yahoo.com>
2022-12-26 00:01:21 +0200

    Initial backend commit

---
+++ medical-record/src/main/java/com/iruzhev/cscb869/medicalrecord/MedicalRecordApplication.java
@@ -0,0 +1,13 @@
+package com.iruzhev.cscb869.medicalrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordApplication.class, args);
+	}
+
+}
---
+++ medical-record/src/test/java/com/iruzhev/cscb869/medicalrecord/MedicalRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iruzhev.cscb869.medicalrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
